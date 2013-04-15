6ac5ef2fc6fbc313b4cd16f44debf0f44360ab20
Jean-Philippe Bidegain <jp@bidega.in>
2013-04-15 19:28:56 +0200

    [-] Correction des positions et tailles de blocs

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -29,4 +29,37 @@ angular.module('roadburnCalendarApp', ['LocalStorageModule'])
       }
       return hour+':'+minute;
     };
-  });
+  })
+  .filter('getPosition', function() {
+    return function(text) {
+      var hour = text.substring(0, 2);
+      var minute = text.substring(2);
+
+      minute = minute * 100 / 60;
+      if (minute === 0) {
+        minute = '00';
+      }
+
+      return hour+minute-1400+20+10;
+    };
+  })
+   .filter('getHeight', function() {
+    return function(end, start) {
+      var endHour = end.substring(0, 2);
+      var endMinute = end.substring(2);
+      var startHour = start.substring(0, 2);
+      var startMinute = start.substring(2);
+
+      endMinute = endMinute * 100 / 60;
+      if (endMinute === 0) {
+        endMinute = '00';
+      }
+      startMinute = startMinute * 100 / 60;
+      if (startMinute === 0) {
+        startMinute = '00';
+      }
+
+      return (endHour+endMinute)-(startHour+startMinute);
+    };
+  })
+  ;
